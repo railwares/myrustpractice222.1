199e45b1f3694b23d6f020b22f51a065b2cbc66a
railwares <150970421+railwares@users.noreply.github.com>
2024-09-18 12:34:37 +0300

    Delete chapter03.rs

--- chapter03.rs
+++
@@ -1,9 +0,0 @@
-#[test]
-fn task1() {
-    let x: i32 = 5; // Uninitialized but used, ERROR !
-    let _y: i32; // Uninitialized but also unused, only a Warning !
-
-    assert_eq!(x, 5);
-    println!("Success!");
-}
-
